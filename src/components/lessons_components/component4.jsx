bfe6f76a3eef2ff8af61c4d6a5facfe49694af94
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-12-07 21:22:30 +0300

    refactor

--- src/components/lessons_components/component4.jsx
+++ src/components/lessons_components/component4.jsx
@@ -2,35 +2,77 @@ import React, { useState } from "react";
 import Wrapper from "./wrapper";
 import { useMargedState } from '../../hooks/hooks';
 
+const add = ({ valuea: a, valueb: b }) => a + b;
+const subtract = ({ valuea: a, valueb: b }) => a - b;
+const devide = ({ valuea: a, valueb: b }) => a / b;
+const multiply = ({ valuea: a, valueb: b }) => a * b;
+
+
 function Component4 () {
   const initialState = {
-    rightValue: 10,
-    leftValue: 20,
+    valuea: 10,
+    valueb: 20,
+  };
+  const [action, setAction] = useState(() => () => null);
+  const [sign, setSign] = useState(null);
+  const applyAction = (f, s) => () => {
+    setSign(s);
+    setAction(() => f);
   };
   const [state, updateState] = useMargedState(initialState);
+  const onChange = (setter, key) => (value) => setter({ [key]: parseInt(value.target.value || 0)});
 
   return (
     <Wrapper>
-      <div>Lesson with MergedState Hook</div>
+      <div>Lesson with functions in state</div>
+
       <div style={{ display: 'flex', justifyContent: 'row' }}>
         <div style={{marginRight: '10px'}}>
-          <div>Left value: {state.leftValue}</div>
-          <div>
-            <button onClick={() => updateState({leftValue: state.leftValue + 1 })}>Up</button>
-          </div>
-          <div>
-            <button onClick={() => updateState({leftValue: state.leftValue - 1 })}>Down</button>
-          </div>
+          <button
+            style={{background: sign === '+' && 'green'}}
+            onClick={applyAction(add, '+')}>ADD</button>
         </div>
-        <div>
-          <div>Right value: {state.rightValue}</div>
-          <div>
-            <button onClick={() => updateState({rightValue: state.rightValue + 1 })}>Up</button>
-          </div>
-          <div>
-            <button onClick={() => updateState({rightValue: state.rightValue - 1 })}>Down</button>
-          </div>
+        <div style={{marginRight: '10px'}}>
+          <button
+            style={{background: sign === '-' && 'green'}}
+            onClick={applyAction(subtract, '-')}>MINUS</button>
         </div>
+        <div style={{marginRight: '10px'}}>
+          <button
+            style={{background: sign === '/' && 'green'}}
+            onClick={applyAction(devide, '/')}
+          >DEVIDE</button>
+        </div>
+        <div style={{marginRight: '10px'}}>
+          <button
+            style={{background: sign === '*' && 'green'}}
+            onClick={applyAction(multiply, '*')}
+          >MULTIPLY</button>
+        </div>
+      </div>
+      <div style={{ marginTop: 10 }}>
+        <label htmlFor="value_a">Value a</label>
+        <input
+          name="valuea"
+          value={state.valuea}
+          type="number"
+          onChange={onChange(updateState, 'valuea')}
+        />
+        <label htmlFor="value_b">Value b</label>
+        <input
+          name="valueb"
+          value={state.valueb}
+          type="number"
+          onChange={onChange(updateState, 'valueb')}
+        />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        <label htmlFor="result">Result</label>
+        <input
+          name="result"
+          value={action(state)}
+          type="number"
+        />
       </div>
     </Wrapper>
   )

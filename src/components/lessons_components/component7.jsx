236343420c1b58c167de8c1f3e88a57f82c79de9
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-12-12 19:59:30 +0300

    lesson 7

--- src/components/lessons_components/component7.jsx
+++ src/components/lessons_components/component7.jsx
@@ -2,30 +2,29 @@ import React, { useRef, useState, useCallback } from "react";
 import Wrapper from "./wrapper";
 
 function useUpdateEffect(callback) {
-  callback();
+  const count = useRef(0)
+  if (count.current === 5) {
+    callback();
+  }
+  count.current += 1;
 }
 
 function Component7 () {
   const [count, setCount] = useState(0);
   const refContainer = useRef();
 
-const makeFocus = useCallback(() => {
-  refContainer.current.focus();
-}, [refContainer]);
+  const makeFocus = useCallback(() => {
+    refContainer.current.focus();
+    console.log('here i"am!!!')
+  }, [refContainer]);
 
-const callback = useCallback(() => {
-  if (count === 5) {
-    makeFocus();
-  }
-}, [count, makeFocus]);
-
-useUpdateEffect(callback);
+  useUpdateEffect(makeFocus);
 
   return (
     <Wrapper>
       <div>Lesson 7 useRef</div>
       <div>Renders: {count}</div>
-      <input ref={refContainer} value="20-th render" />
+      <input ref={refContainer} defaultValue="20-th render" readOnly={true} />
       <button onClick={() => setCount(count + 1)}>Render</button>
     </Wrapper>
   )
